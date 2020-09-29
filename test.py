064837670680d4e09c09354bbfefff2f6ae8d257
BdsvhPierre <BdsvhPierre@users.noreply.github.com>
2020-09-29 18:48:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,24 @@
 
 # Faire un algorithm qui prend une liste en paramètre, et qui crée une nouvelle liste avec les valeurs unique de la première : [1,1,1,3,2,2,3] -> [1, 3, 2]
 
+l2 = []
+
 l = [1, 1, 1, 3, 2, 2, 3]
 count = 0
-   for value in l:
-        if value == 1:
-            count = count + 1
-            if count == 1:
-                l2.append(value)
-            break
-    print(l2)
+for value in l:
+    if value == 1:
+        count = count + 1
+        if count == 3:
+            l2.append(value)
+        
+    if value == 3:
+        count = count + 1
+        if count == 4:
+            l2.append(value)
+        
+    if value == 2:
+        count = count + 1
+        if count == 6:
+            l2.append(value)
+        
+print(l2)
