2bfe2b7ccd584069cd2f80e953486a5258791898
BdsvhPierre <BdsvhPierre@users.noreply.github.com>
2020-09-29 18:28:12 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+# Faire un algorithme qui prend une liste en paramètre et en crée une copie inversé :
+# [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+
+# Faire un algorithm qui prend une liste en paramètre, et qui crée une nouvelle liste avec les valeurs unique de la première : [1,1,1,3,2,2,3] -> [1, 3, 2]
+
+l = [1, 1, 1, 3, 2, 2, 3]
+count = 0
+   for value in l:
+        if value == 1:
+            count = count + 1
+            if count == 1:
+                l2.append(value)
+            break
+    print(l2)
